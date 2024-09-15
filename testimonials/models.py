16b064d20be0aedac27b61c4309a60c24db52e16
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-09-15 20:57:30 +0000

    FIX editing testimonials

--- testimonials/models.py
+++ testimonials/models.py
@@ -7,12 +7,17 @@ class Testimonial(models.Model):
     content = models.TextField()
     rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
     created_at = models.DateTimeField(default=timezone.now, editable=False)
-    updated_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='testimonials', null=True, blank=True)
 
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        if self.pk and self.created_at != timezone.now():
+            self.updated_at = timezone.now()
+        super().save(*args, **kwargs)
+
     @property
     def was_edited(self):
-        return self.created_at != self.updated_at
+        return self.updated_at and self.updated_at != self.created_at
