9bf702a9f659f5b8f39b9bcceb395ab015f96450
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-09-25 07:27:12 +0000

    Remove offset/ajax for load testimonials - update sorting

--- testimonials/views.py
+++ testimonials/views.py
@@ -14,46 +14,28 @@ import json
 
 # all testimonials
 
+
 def testimonial_list(request):
     sort_option = request.GET.get('sort', 'date')
-    offset = int(request.GET.get('offset', 4))  # Default to 4 testimonials
-    testimonials = Testimonial.objects.annotate(num_comments=Count('comments'))
-
+    
     if sort_option == 'views':
-        testimonials = testimonials.order_by('-views_count', '-num_comments', '-rating')
+        testimonials = Testimonial.objects.annotate(
+            num_comments=Count('comments')
+        ).order_by('-views_count', '-num_comments', '-rating')
     elif sort_option == 'comments':
-        testimonials = testimonials.order_by('-num_comments', '-views_count', '-rating')
+        testimonials = Testimonial.objects.annotate(
+            num_comments=Count('comments')
+        ).order_by('-num_comments', '-views_count', '-rating')
     elif sort_option == 'rating':
-        testimonials = testimonials.order_by('-rating', '-views_count', '-num_comments')
+        testimonials = Testimonial.objects.annotate(
+            num_comments=Count('comments')
+        ).order_by('-rating', '-views_count', '-num_comments')
     else:  # Default sorting by date
-        testimonials = testimonials.order_by('-created_at', '-views_count', '-num_comments', '-rating')
+        testimonials = Testimonial.objects.annotate(
+            num_comments=Count('comments')
+        ).order_by('-created_at', '-views_count', '-num_comments', '-rating')
 
-    testimonials = testimonials[:offset]
-
-    total_count = Testimonial.objects.count()
-
-    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-        return JsonResponse({
-            'testimonials': [
-                {
-                    'id': testimonial.id,
-                    'name': testimonial.name,
-                    'content': testimonial.content,
-                    'views_count': testimonial.views_count,
-                    'comments_count': testimonial.comments.count(),
-                    'rating': testimonial.rating,
-                    'created_at': testimonial.created_at.strftime('%Y-%m-%d %H:%M:%S'),
-                }
-                for testimonial in testimonials
-            ],
-            'total_count': total_count,
-        })
-
-    return render(request, 'testimonials/testimonial_list.html', {
-        'testimonials': testimonials,
-        'offset': offset,
-        'total_count': total_count,
-    })
+    return render(request, 'testimonials/testimonial_list.html', {'testimonials': testimonials})
 
 
 
